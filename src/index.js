a4c5e8f6c2944ed3cf51bd57cc2c56edf9bccdf1
David <davidsinghrolson@gmail.com>
2025-03-18 19:59:49 +0530

    login logout done with added refresh token and subscription model created

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ dotenv.config();
 import { app } from './App.js';
 import connectDB from "./db/index.js";
 
+dotenv.config({
+    path: './.env'
+})
 connectDB().then(()=>{
     app.listen(process.env.PORT || 8000,()=>{
         console.log(`server is running at port ${process.env.PORT}`);
